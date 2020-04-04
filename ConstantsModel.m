ce5f1ec55840405b1a677f9e0d323de347dcd205
Mahdi Mohajeri <Mahdi Mohajeri@yahoo.com>
2020-04-04 09:26:27 +0430

    For conflict

--- ConstantsModel.m
+++ ConstantsModel.m
@@ -6,7 +6,7 @@ dt = 0.0256;                     % [m]                              Diameter of 
 dp = 0.0082;                     % [m]                              Diameter of catalyts particle
 epsilon = 0.48;                  % [m^3/m^3]                        Porosity
 Density_bed = 50*(1000);         % [kg/m^3][g/m^3]                  Density of reactor bed
-Phis = 1;                        % [unitless]                       Sphericity factor for spheres catalyts particles 
+Phis = 1;                        % [unitless]                       Sphericity factor for spheres catalyts particles
 as = 6*(1-epsilon)/(Phis*dp);    % [m^2/m^3]                        External surface to particle volume ratio
 
 %% Defining the require constans of operation conditions
@@ -15,27 +15,27 @@ P = 1*(101325);                  % [atm][Pa]                        Pressure of 
 Tb = 450+(273.15):600+(273.15);  % [oC][K]                          Temperature of reactor coolant
 T0 = 300+(273.15);               % [oC][K]                          Temperature of inlet reactor
 Rep = 1400;                      % [unitless]                       Reynolds number
-Flowin = 4*(1/3600);             % [Nm^3/h][Nm^3/s]                 Inlet volume flowrate 
+Flowin = 4*(1/3600);             % [Nm^3/h][Nm^3/s]                 Inlet volume flowrate
 C_ethan = 1:2;                   % [%mol]                           Mole frac of inlet ethane
 c_air = 98:99;                   % [%mol]                           Mole frac of inlet Air
 
 %% Defining the require constans of transfer parameters
 
-Deffr = 32*(1/3600);             % [m^2/h][m^2/s]                   Effective mass transfer 
-                                 %                                  coefficient in radius direction.
-Deffz = 53*(1/3600);             % [m^2/h][m^2/s]                   Effective mass transfer coefficient 
-                                 %                                  in horizontal axis direction.
+Deffr = 32*(1/3600);             % [m^2/h][m^2/s]                   Effective mass transfer
+%                                  coefficient in radius direction.
+Deffz = 53*(1/3600);             % [m^2/h][m^2/s]                   Effective mass transfer coefficient
+%                                  in horizontal axis direction.
 kg = 576*(1/3600);               % [m^3/(m^2*h)][m^3/(m^2*s)]       Surface mass transfer coefficient
 hg = 928.8*(1000)*(1/3600);      % [kJ/(m^2*h*K)][J/(m^2*s*K)]      Surface heat transfer coefficient
-keffr = 9.72*(1000)*(1/3600);    % [kJ/(m*h*K)][J/(m*s*K)]          Effective thermal conductivity 
-                                 %                                  in the radius direction.
+keffr = 9.72*(1000)*(1/3600);    % [kJ/(m*h*K)][J/(m*s*K)]          Effective thermal conductivity
+%                                  in the radius direction.
 hw = 1051.2*(1000)*(1/3600);     % [kJ/(m^2*h*K)][J/(m^2*s*K)]      Wall heat transfer coefficient
 
-%% Defining the require constants of components properties 
+%% Defining the require constants of components properties
 %
 % Units
 %
-% Mw: [g/mol]           Tc: [K]       Pc: [Pa]      
+% Mw: [g/mol]           Tc: [K]       Pc: [Pa]
 % cp_R: [unitless depend on R]        deltaS0: [J/(mol*K)]      deltaH0:[kJ/mol][J/mol]
 % R=8.314 (J/mol*K)
 
@@ -62,13 +62,13 @@ components = [C2H6 C2H4 O2 CO2 CO H2O];
 % component order list: [C2H6 C2H4 O2 CO2 CO H2O]
 
 RnxKinetic = struct('Aprime',[4.95 1.35 1.76 2.61 2.16]*(1/1000)*(1/3600),...
-    'EnergyA', [7.55e01 5.24e01 1.43e02 1.10e02 8.80e01]*(1000),...  
+    'EnergyA', [7.55e01 5.24e01 1.43e02 1.10e02 8.80e01]*(1000),...
     'm', [1 5.45e-02 1.07 1.71e-01 5.38e-01],...
-    'vcoffrxn', [-1 1 -0.5 0 0 1; -1 0 -3.5 2 0 3; -1 0 -2.5 0 2 3; 0 -1 -3 2 0 2; 0 -1 -2 0 2 2]); 
+    'vcoffrxn', [-1 1 -0.5 0 0 1; -1 0 -3.5 2 0 3; -1 0 -2.5 0 2 3; 0 -1 -3 2 0 2; 0 -1 -2 0 2 2]);
 
-%% Defining the require constants for 
+%% Defining the require constants for
 
-%% Calculation 
+%% Calculation
 Nz = 5; % No. of interior point in z direction
 Nr = 3; % No. of interior point in r direction
 zmin = 0; zmax = 1;
@@ -82,27 +82,20 @@ Lz = sym(ones(numel(z_nodes),1));
 for i=1:numel(z_nodes)
     for j=1:numel(z_nodes)
         if j~=i
-<<<<<<< HEAD
-            Lz(i,1) = (x-z_nodes(j))/(z_nodes(i)-z_nodes(j))*Lz(i,1);
-=======
             Lz(i,1) = (z-z_nodes(j))/(z_nodes(i)-z_nodes(j))*Lz(i,1);
-
             % Lz is Lagrange interpolation polynomial in z direction
         end
-    end 
+    end
 end
 syms r
 Lr = sym(ones(numel(r_nodes),1));
 for i=1:numel(r_nodes)
     for j=1:numel(r_nodes)
         if j~=i
-<<<<<<< HEAD
-            Lr(i,1) = (x-r_nodes(j))/(r_nodes(i)-r_nodes(j))*Lr(i,1);
-=======
             Lr(i,1) = (r-r_nodes(j))/(r_nodes(i)-r_nodes(j))*Lr(i,1);
-	    % Lr is Lagrange interpolation polynomial in r direction
+            % Lr is Lagrange interpolation polynomial in r direction
         end
-    end 
+    end
 end
 Lz_prime = diff(Lz);      % First drivative of Lagrange polynomial in z direction
 Lz_Zegond = diff(Lz,2);   % Second drivative of Lagrange polynomial in z direction
@@ -113,8 +106,8 @@ Bz = zeros(numel(z_nodes));
 for i = 1:numel(z_nodes)
     for j = 1:numel(z_nodes)
         Az(i,j) = double(subs(Lz_prime(j),z_nodes(i)));
-        Bz(i,j) = double(subs(Lz_Zegond(j),z_nodes(i))); 
-    end    
+        Bz(i,j) = double(subs(Lz_Zegond(j),z_nodes(i)));
+    end
 end
 Ar = zeros(numel(r_nodes));
 Br = zeros(numel(r_nodes));
@@ -122,7 +115,7 @@ for i = 1:numel(r_nodes)
     for j = 1:numel(r_nodes)
         Ar(i,j) = double(subs(Lr_prime(j),r_nodes(i)));
         Br(i,j) = double(subs(Lr_Zegond(j),r_nodes(i)));
-    end    
+    end
 end
 
 
